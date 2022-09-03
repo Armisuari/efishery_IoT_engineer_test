1ad39921c316ecb0b61b9da3f7dc58aae9bfdb78
Armisuari <ariefnurmisuari@gmail.com>
2022-09-04 00:33:00 +0800

    adding RTC

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,32 @@
+#include <Arduino.h>
+#include "DS3231.h"
 #include "DS18b20.h"
 #include "TDS.h"
 
 TDS myTDS(27);
+String payload;
 
 void setup()
 {
   Serial.begin(115200);
+  setup_time();
   sensors.begin();
   // pinMode(TdsSensorPin,INPUT);
 
   while (1)
   {
+    DateTime now = rtc.now();
     sensors.requestTemperatures();
     float temperature = sensors.getTempCByIndex(0);
+
+    payload = String(daysOfTheWeek[now.dayOfTheWeek()]) + "@" + String(now.day()) + "@" + String(now.month()) + "@" + String(now.year()) +
+           "#" + String(temperature,1) + "#" + String(myTDS.read(temperature),0);
+
+    Serial.println(payload);
+    // printf(" %s\n", data);
+    printf("Date Time: %s,%d-%d-%d >> ", daysOfTheWeek[now.dayOfTheWeek()], now.day(), now.month(), now.year());
     printf("Temperature: %.1f ºC, TDS: %.f ppm\n", temperature, myTDS.read(temperature));
+
     delay(500);
   }
 }
